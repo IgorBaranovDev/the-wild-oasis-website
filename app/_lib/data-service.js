c7d3892e24ade8cbad7b2f9a8c2ed501969b3ec6
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-09-08 16:43:52 +0200

    feat: dynamic rote for cdbin psge

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -11,9 +11,6 @@ export async function getCabin(id) {
     .eq("id", id)
     .single();
 
-  // For testing
-  // await new Promise((res) => setTimeout(res, 1000));
-
   if (error) {
     console.error(error);
   }
