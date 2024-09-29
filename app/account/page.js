930421bad482f29c6657ee7596aec518239ba3fb
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-09-29 21:43:46 +0200

    feat:implemented sign in page

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,20 @@
+import { Span } from "next/dist/trace";
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
     <div>
-      <h2 className="font-semibold text-2xl text-accent-400 mb-7">Welcome,</h2>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome,{" "}
+        <span className=" text-transform: capitalize">{firstName}</span>
+      </h2>
     </div>
   );
 }
